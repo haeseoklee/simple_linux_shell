583e8a60bfd36d7c667f29a8cf52960436cda18b
haeseoklee <haeseoklee.dev@gmail.com>
2020-03-29 12:37:49 +0900

    add main.c smsh.h

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "smsh.h"
+
+int main()
+{
+    puts("Hello!");
+    return 0;
+}
---
+++ smsh.h
@@ -0,0 +1,8 @@
+#ifndef __SMSH_H__
+#define __SMSH_H__
+
+#include <stdio.h>
+#include <string.h>
+
+
+#endif
